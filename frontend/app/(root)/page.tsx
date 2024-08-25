1322d67d544336b0087352bb52dbde8873fe11bc
Mikey Sharma <mikeysharma99@gmail.com>
2024-08-25 21:34:14 +0545

    change static data to dynamic data from database

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,32 +1,41 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-export default async function Home() {
+export default async function Home({ searchParams: { id, page } }: SearchParamProps) {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+  
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-         <HeaderBox
-         type="greeting"
-         title="Welcome"
-         user={loggedIn?.name || "Guest"}
-         subtext="Access and manage your account and transactions efficiently."
-         />
-         <TotalBalanceBox
-         accounts={[]}
-         totalBanks={1}
-         totalCurrentBalance={1250.35}
-         />
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
         </header>
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar 
-      user={loggedIn}
-      transactions={[]}
-      banks={[{currentBalance : 123.50}, { currentBalance : 500}]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)} />
     </section>
   );
 }
