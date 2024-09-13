fc22731dfed46baebdbd759177926c6b13558379
Mikey Sharma <mikeysharma99@gmail.com>
2024-09-13 20:58:17 +0545

    add force-dynamic

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"], variable : "--font-inter" });
 
 const IBMPlexSerif = IBM_Plex_Serif({subsets : ["latin"], weight: ["400", "700"], variable : "--font-ibm-plex-serif"});
--- frontend/components/ui/progress.tsx
+++ frontend/components/ui/progress.tsx
@@ -5,9 +5,14 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+// Define the type for the props, extending the original props from Radix UI's ProgressPrimitive.Root
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ProgressProps
 >(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
